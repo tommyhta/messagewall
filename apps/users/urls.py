5345e9d646ec6e33e1da65eff453078375d70ae5
Tommy <tommy@Tommys-MacBook-Pro.local>
2018-07-21 17:31:15 -0700

    new project entirely

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -3,15 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^breached$',views.breached),
-    url(r'^logorreg$', views.logorreg),
     url(r'^register$', views.register),
-    url(r'^welcome$', views.welcome),
     url(r'^login$', views.login),
+    url(r'^welcome$', views.welcome),
     url(r'^logout$', views.logout),
-    url(r'^post$', views.post),
-    url(r'^delpost$', views.delpost),
-    url(r'^comment$',views.comment),
-    url(r'^delcom$',views.delcom)
-
+    url(r'^breached$', views.breached)
 ]
